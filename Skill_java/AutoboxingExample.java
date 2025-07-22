08454dd582ddd309483ab07e2623d9a7ae686a73
Bhuvan <chbhuvan14@gmail.com>
2025-07-22 14:34:18 +0530

    Delete Skill_java directory

--- Skill_java/AutoboxingExample.java
+++
@@ -1,9 +0,0 @@
-public class AutoboxingExample {
-    public static void main(String[] args) {
-        int primitiveNumber = 100;
-        Integer wrapperNumber = primitiveNumber;
-        System.out.println("Autoboxing to Integer:" + wrapperNumber);
-        int newPrimitive = wrapperNumber;
-        System.out.println("Unboxing to int:" + newPrimitive);
-    }
-}
--- Skill_java/ParsingExample.java
+++
@@ -1,9 +0,0 @@
-public class ParsingExample {
-    public static void main(String[] args) {
-        String textNumber = "500";
-        int realNumber = Integer.parseInt(textNumber);
-        System.out.println("Original text: " + textNumber);
-        System.out.println("Converted number: " + realNumber);
-        System.out.println("Output of Math: " + (realNumber / 5));
-    }
-}
